abb650e3e2b5742215c24f2c4deaa191d2f577c5
Denis Fomin <deniska.tver.69@gmail.com>
2025-06-23 21:43:54 +0300

    operations with numbers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) { System.out.println("Введите текст и нажмите <Enter>: ");
- String text = new Scanner(System.in).nextLine();
- System .out.println("Длина текста: "+text.length());
+    public static void main(String[] args) {
+        System.out.println("Введите первое число");
+        int numberOne = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int numberTwo = new Scanner(System.in).nextInt();
+
+        int sum = numberOne+numberTwo;
+        System.out.println("Сумма чисел равна: " + sum);
+        int diff = numberOne-numberTwo;
+        System.out.println("Разность чисел равна: " + diff);
+        int multiply= numberOne*numberTwo;
+        System.out.println("Произведение чисел равно: " + multiply);
+        double quotient = (double) numberOne / numberTwo;
+        System.out.println("Частное: " + quotient);
     }
 }
