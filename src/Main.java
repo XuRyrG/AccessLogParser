5f22b0a36a32a5e8bf2120603f416038e59a28ed
Denis Fomin <deniska.tver.69@gmail.com>
2025-08-22 17:50:11 +0300

    total, min and max lenght of line.add new file LineTOOlongException

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,48 @@
-import java.io.File;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) { System.out.println("Введите текст и нажмите <Enter>: ");
- String text = new Scanner(System.in).nextLine();
- System .out.println("Длина текста: "+text.length());
-
+    public static void main(String[] args) throws IOException {
         int N = 1;
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
+
             if ((!fileExists) || (isDirectory)) {
                 System.out.println("Файл не существует или указанный путь является путём к папке, а не к файлу");
                 continue;
             }
             System.out.println("Путь указан верно. Кол-во верно указанных путей к файлу: " + N);
             N++;
+
+            int minLength = Integer.MAX_VALUE;
+            int maxLength = Integer.MIN_VALUE;
+            int lineCounter = 0;
+            String line;
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    if (line.length() > 1024) {
+                        throw new LineTooLongException("Длинная строка (Строка >= 1024 символа).");
+                    }
+                    if (line.length() < minLength) {
+                        minLength = line.length();
+                    } else if (line.length() >= maxLength) {
+                        maxLength = line.length();
+                    }
+                    lineCounter++;
+                }
+                System.out.println("Общее кол-во строк в файле: " + lineCounter);
+                System.out.println("Длина самой короткой строки: " + minLength);
+                System.out.println("Длина самой длинной строки: " + maxLength);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
